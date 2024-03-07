0aeafc282d7648e74a58cedea52b29c3afad123a
Fiido <roccobosio@gmail.com>
2024-03-07 11:47:09 -0300

    config .prettierrc

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core"
+import { AppModule } from "./app.module"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 3000;
+  const app = await NestFactory.create(AppModule)
+  const PORT = process.env.PORT || 3000
 
-  await app.listen(PORT);
-  console.log('app listen on port', PORT);
+  await app.listen(PORT)
+  console.log("app listen on port", PORT)
 }
-bootstrap();
+bootstrap()
