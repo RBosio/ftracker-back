b8c65ee6c3c9c3b2122e4dad87d16281f0f68690
Fiido <roccobosio@gmail.com>
2024-03-07 10:47:12 -0300

    config typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      host: 'db',
+      type: 'mysql',
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
