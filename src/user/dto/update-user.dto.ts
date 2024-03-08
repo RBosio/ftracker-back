e0cf426bfba638b932c6af864094232a88420b60
Fiido <roccobosio@gmail.com>
2024-03-08 13:35:50 -0300

    user docs

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,5 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger"
+
 export class UpdateUserDto {
-  name?: string
-  surname?: string
-  password?: string
+  @ApiProperty({
+    name: "name",
+    type: "string",
+    example: "Bruce",
+    required: false,
+  })
+  name: string
+  @ApiProperty({
+    name: "surname",
+    type: "string",
+    example: "Wayne",
+    required: false,
+  })
+  surname: string
+  @ApiProperty({
+    name: "password",
+    type: "string",
+    example: "$uperp4ssw0rd",
+    required: false,
+  })
+  password: string
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,32 +6,84 @@ import {
   Patch,
   Param,
   Delete,
+  HttpStatus,
 } from "@nestjs/common"
 import { UserService } from "./user.service"
 import { UpdateUserDto } from "./dto/update-user.dto"
 import { CreateUserDto } from "./dto/create-user.dto"
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from "@nestjs/swagger"
 
+@ApiTags("user")
 @Controller("user")
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiOperation({ summary: "create user" })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: "user created",
+  })
+  @ApiBody({ type: CreateUserDto })
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto)
   }
 
   @Get()
+  @ApiOperation({ summary: "get users" })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: "get all users",
+  })
   findAll() {
     return this.userService.findAll()
   }
 
-  @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.userService.findOne(+id)
+  @Get(":userId")
+  @ApiOperation({ summary: "get user" })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: "get user",
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: "user not found",
+  })
+  @ApiParam({
+    name: "userId",
+    type: "number",
+    example: 1,
+  })
+  findOne(@Param("userId") userId: string) {
+    return this.userService.findOne(+userId)
   }
 
-  @Patch(":id")
-  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto)
+  @Patch(":userId")
+  @ApiOperation({ summary: "update user" })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: "update user",
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: "user not found",
+  })
+  @ApiParam({
+    name: "userId",
+    type: "number",
+    example: 1,
+  })
+  @ApiBody({ type: UpdateUserDto })
+  update(
+    @Param("userId") userId: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.update(+userId, updateUserDto)
   }
 }
