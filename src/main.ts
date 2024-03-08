dbcb485d8ed42f8457fdde6db6ba323d57266e47
Fiido <roccobosio@gmail.com>
2024-03-08 13:24:23 -0300

    init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const PORT = process.env.PORT || 3000
+
+  const config = new DocumentBuilder()
+    .setTitle("FTracker")
+    .setVersion("1.0")
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("api", app, document)
 
   await app.listen(PORT)
   console.log("app listen on port", PORT)
